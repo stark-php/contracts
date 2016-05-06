56a932b0eea1c1653202e85afc1163d170c8334a
Christopher Rolfe <christopher@thedistrict.co.uk>
2016-05-06 19:05:44 +0100

    Making it so that you pass through the request type when processing a route

--- lib/Router/RouterInterface.php
+++ lib/Router/RouterInterface.php
@@ -2,6 +2,7 @@
 
 interface RouterInterface
 {
-    public function processRoute(string $url);
+    public function getRoutes();
+    public function processRoute(string $request_type, string $request_url);
     public function storeRoute(string $request_type, string $request_url, $function_to_process_route);
 }
